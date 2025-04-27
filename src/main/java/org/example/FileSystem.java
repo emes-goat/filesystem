546231fbba006b365ccf746fac4ff4d2aaf56ca1
emes <michalsieniawski1@gmail.com>
2025-04-27 14:00:11 +0200

    Initial commit

---
+++ src/main/java/org/example/FileSystem.java
@@ -0,0 +1,79 @@
+package org.example;
+
+import com.google.common.collect.Lists;
+import com.google.common.primitives.Bytes;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.*;
+
+public class FileSystem {
+
+    private final Integer DISK_SIZE = 5 * 1024 * 1024;
+    private final Integer SECTOR_SIZE = 4 * 1024;
+    private final Integer SECTOR_COUNT = DISK_SIZE / SECTOR_SIZE;
+
+    private Map<Integer, byte[]> sectors = new HashMap<>();
+    private Map<Integer, byte[]> sectorHashes = new HashMap<>();
+    private Set<Integer> takenSectors = new HashSet<>();
+
+    private Map<UUID, Set<Integer>> files = new HashMap<>();
+
+    private MessageDigest digest = MessageDigest.getInstance("SHA-256");
+
+    public FileSystem() throws NoSuchAlgorithmException {
+    }
+
+    public void save(UUID path, byte[] content) {
+        int requiredSectors = (int) Math.ceil((double) content.length / (double) SECTOR_SIZE);
+        List<List<Byte>> chunkedContent = Lists.partition(Bytes.asList(content), SECTOR_SIZE);
+        List<Integer> assignedSectors = findAvailableSectors(requiredSectors);
+
+        if (chunkedContent.size() != assignedSectors.size()) {
+            throw new RuntimeException("Chunked content size mismatch");
+        }
+
+        for (int i = 0; i < chunkedContent.size(); i++) {
+            byte[] sectorBytes = Bytes.toArray(chunkedContent.get(i));
+            byte[] hashSha256 = calculateSha256(sectorBytes);
+
+            sectors.put(assignedSectors.get(i), sectorBytes);
+            sectorHashes.put(assignedSectors.get(i), hashSha256);
+        }
+
+        files.put(path, new HashSet<>(assignedSectors));
+    }
+
+    private List<Integer> findAvailableSectors(int requiredSectors) {
+        List<Integer> assignedSectors = new ArrayList<>(requiredSectors);
+
+        int sectorNumber = 0;
+        while (assignedSectors.size() < requiredSectors) {
+            if (sectorNumber >= SECTOR_COUNT) {
+                throw new IllegalStateException("Not enough disk space");
+            }
+
+            if (!takenSectors.contains(sectorNumber)) {
+                assignedSectors.add(sectorNumber);
+                takenSectors.add(sectorNumber);
+            }
+
+            sectorNumber++;
+        }
+
+        return assignedSectors;
+    }
+
+    private byte[] calculateSha256(byte[] content) {
+        digest.reset();
+        digest.update(content);
+        return digest.digest();
+    }
+
+    public byte[] read(UUID path) {
+        // bierzemy sektory pliku z files
+        // czytamy sektory i weryfikujemy hasze
+        // sklejamy sektory i zwracamy
+        return null;
+    }
+}
